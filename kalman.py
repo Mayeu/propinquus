4c7dab294fab7779483d7c4950b622d8553639fb
Mayeutik <mayeu.tik@gmail.com>
2011-07-19 10:59:07 +0200

    Modify the filter for tail data

--- kalman.py
+++ kalman.py
@@ -25,8 +25,9 @@ def extract_data(line):
 
     return data
 
+training_file = 'tail_muscles_size.dat'
 
-f = open('training_sequence.dat','r')
+f = open(training_file,'r')
 line = f.readline()
 
 k = len(line.strip("\n").split(' '))
@@ -43,10 +44,12 @@ f.close()
 #    [[1],[0],[0]]
 #    ])
 #print I_t
-U_barre_t = np.random.random_sample((k,n))
+U_barre_t = np.random.random_integers(0, 10, (k,n)) * 0.001
+#U_barre_t = np.zeros((k,n))
 #print 'U_barre_t:'
 #print U_barre_t
-V_barre_t_1 = np.random.random_sample((n,n))
+V_barre_t_1 = np.random.random_integers(0, 10, (n,n)) * 0.001
+#V_barre_t_1 = np.zeros((k,n))
 #print 'V_barre_t_1:'
 #print V_barre_t_1
 r_opt_t_1 = np.zeros((n,1))
@@ -54,11 +57,15 @@ r_opt_t_1 = np.zeros((n,1))
 alpha = 1
 beta = 1
 
-t = 60
+t = 100
 
 for i in range(t):
-    f = open('training_sequence.dat','r')
+    f = open(training_file,'r')
     fc = 0
+    if i != 0:
+        alpha = alpha / 1.0025
+        beta = beta / 1.0025
+
     for line in f:
         I_t = extract_data(line)
         #print 'I_t:'
@@ -66,10 +73,7 @@ for i in range(t):
         if i != 0 or fc != 0 :
             U_barre_t = U_opt_t
             V_barre_t_1 = V_opt_t_1
-            r_prime_t = r_opt_t
-
-            alpha = alpha / 1.0025
-            beta = beta / 1.0025
+            r_opt_t_1 = r_opt_t
 
     # Filter equation for learning
         r_prime_t = np.dot(V_barre_t_1, r_opt_t_1) # eq 21
@@ -96,52 +100,66 @@ for i in range(t):
         fc += 1 
     f.close()
 
-print 'U_opt_t: '
-print U_opt_t
+#print 'U_opt_t: '
+#print U_opt_t
 print 'V_opt_t_1: '
 print V_opt_t_1
 
 U = U_opt_t
 V = V_opt_t_1
-r_opt_t_1 = np.zeros((k,1))
+r_opt_t_1 = np.zeros((n,1))
 
 
 f = open('predicting_sequence.dat','r')
 fpg = open('predicting_graph.dat','w')
 fig = open('input_graph.dat','w')
+fdg = open('delta_graph.dat','w')
 ct = 0
+r_barre_t = np.dot(V, r_opt_t_1) # eq 15
 for line in f:
     I_t = extract_data(line)
 # Filter equation
-    r_barre_t = np.dot(V, r_opt_t_1) # eq 14
     #print 'r_barre_t:'
     #print r_barre_t
 
     #print 'I_t: '
     #print I_t
-    i = 0
+    fig.write(str(ct))
     for el in I_t:
         for e in el:
-            fig.write(str(ct) + ' ' + str(k-i-1) + ' ' + str(e) + "\n")
-        i += 1
+            fig.write(' ' + str(e))
     fig.write("\n")
 
-    r_opt_t = r_barre_t + np.dot(0.2 * U.transpose(), I_t - np.dot(U, r_barre_t))
-    pred = np.dot(U, r_opt_t) # eq 1
+
+    r_opt_t = r_barre_t + 0.2 * np.dot(U.transpose(), (I_t - np.dot(U, r_barre_t))) # eq 14
 
     r_opt_t_1 = r_opt_t
 
+    r_barre_t = np.dot(V, r_opt_t_1) # eq 15
+
+    pred = np.dot(U, r_barre_t) # eq 1
+
     #print 'Prediction: '
     #print np.dot(U, r_opt_t)
-    i = 0
+    #i = 0
+    fpg.write(str(ct))
     for el in pred:
         for e in el:
-            fpg.write(str(ct) + ' ' + str(k-i-1) + ' ' + str(e) + "\n")
-        i += 1
+            fpg.write(' ' + str(e))
     fpg.write("\n")
+
+    # Print delta heat map
+    delta = I_t - pred
+    i = 0
+    for el in delta:
+        for e in el:
+            fdg.write(str(ct) + ' ' + str(i) + ' ' + str(e) + "\n")
+        i += 1
+    fdg.write("\n")
 
     ct += 1
 
 f.close()
 fpg.close()
 fig.close()
+fdg.close()
