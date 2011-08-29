b598b42fdd439cf2a4369ae4f5ce3719e654812e
Mayeutik <mayeu.tik@gmail.com>
2011-08-29 15:18:55 +0200

    Add utility script

---
+++ script/extract_muscles.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <mayeu.tik@gmail.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return. Matthieu Maury
+# ----------------------------------------------------------------------------
+
+import math
+import re
+import numpy
+
+name = 'data_worm.dat'
+nout = 'tail_muscles_size_all.dat'
+
+M = 48
+P = 2*(M+1)
+
+f = open(name, 'rb')
+fout = open(nout, 'w')
+for l in f:
+    #if re.search('T=', l):
+        #fout.write('## ' + l)
+    if re.search('X=', l):
+        worm = numpy.zeros((P,2))
+        coordinate = l.strip(' \t\n\r').split(' ')
+        coordinate.pop(0)
+        i = 0
+        flag = True
+        pos = 0
+        for c in coordinate:
+            if i == 0:
+                #print float(c)
+            # x
+                if flag == True:
+                    worm[pos][0] = float(c)
+                else:
+                    worm[97-pos][0] = float(c)
+            elif i == 1:
+            # y
+                if flag == True:
+                    worm[pos][1] = float(c)
+                else:
+                    worm[97-pos][1] = float(c)
+
+            elif i == 2:
+            #don't care about z
+                if flag == True:
+                    flag = False
+                else:
+                    pos += 1
+                    flag = True
+            i = (i+1)%3
+         # print in the file
+        #for el in worm:
+        #    for c in el:
+        #        fout.write(str(c)+"\t")
+        #    fout.write("\n")
+        for i in range(P):
+            fout.write(str(math.sqrt(
+                math.pow((worm[(i+1)%P][0]-worm[(i)%P][0]),2) +
+                math.pow((worm[(i+1)%P][1]-worm[(i)%P][1]),2))))
+            if i != (P-1):
+                fout.write(' ')
+        fout.write('\n')
---
+++ script/mean_error.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <mayeu.tik@gmail.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return. Matthieu Maury
+# ----------------------------------------------------------------------------
+
+import sys
+import fileinput
+import re
+
+f = open('mean_error.dat','w')
+r = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,4,5,6,7,8,9]
+c = -1
+cf = 0
+ 
+args = sys.argv
+args.pop(0)
+args.sort()
+print args
+
+mean = 0
+for line in fileinput.input(args):
+    if fileinput.isfirstline():
+        c += 1
+        cf = 0
+        if c > 0:
+            f.write(str(r[c-1]) + ' ' + str(mean/(990*98)) + '\n')
+            mean = 0
+    if re.match('# Dataset',line):
+        if cf != 0:
+            f.write(str(r[c]) + ' ' + str(mean/(990*98)) + '\n')
+            mean = 0
+        continue
+    if line == '\n':
+        continue
+
+    line = line.split(' ')
+    #print line
+    mean += float(line.pop())
+    cf += 1
+
+f.write(str(r[c]) + ' ' + str(mean/(990*98)) + '\n')
+f.close()
